1fb298361ead8b78e86dc9700d1036838a855ed8
Ronaldo2017 <{ID}+{username}@users.noreply.github.com>
2023-04-02 15:41:33 -0300

    Incluido comentario, para teste

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,6 +2,7 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
+	/*classe main = principal para execução do projeto*/
 	public static void main(String[] args) {
 		System.out.println("EU SOU DESENVOLVEDOR DE SOFTWARE JAVA");
 	}
