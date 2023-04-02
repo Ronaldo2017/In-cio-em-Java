3d68666d113a0d33d1e5ef45c51f780773066e4e
Ronaldo2017 <{ID}+{username}@users.noreply.github.com>
2023-04-02 15:32:00 -0300

    Iniciando com Java

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,8 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	public static void main(String[] args) {
+		System.out.println("EU SOU DESENVOLVEDOR DE SOFTWARE JAVA");
+	}
+}
